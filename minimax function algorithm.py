e1fe07f2fbf70ce9d8cc78db30363eae92165d3a
Pie <78828566+pie972@users.noreply.github.com>
2022-04-28 22:24:13 +0200

    Add files via upload

---
+++ minimax function algorithm.py
@@ -0,0 +1,18 @@
+function minimax(board, depth, isMaximizingPlayer)
+
+    if current board state is a terminal state 
+        return value of the board
+    
+    if isMaximizingPlayer 
+        bestVal = -INFINITY 
+        for each move in board 
+            value = minimax(board, depth+1, false)
+            bestVal = max( bestVal, value) 
+        return bestVal
+
+    else 
+        bestVal = +INFINITY 
+        for each move in board 
+            value = minimax(board, depth+1, true)
+            bestVal = min( bestVal, value) 
+        return bestVal
